28460fe4e9042f6743b6a0853b2dd9a0c158ee17
DESKTOP-9QK8KIU\user1 <yerinsong05@gmail.com>
2024-06-24 16:29:30 +0900

    초기설정

---
+++ groupware/src/main/java/smile/office/groupware/GroupwareApplication.java
@@ -0,0 +1,15 @@
+package smile.office.groupware;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan
+public class GroupwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupwareApplication.class, args);
+	}
+
+}
