5900048eed4bc3d680302a153687646810cd0bf5
DESKTOP-BRA060G\user1 <jisoou4@gmail.com>
2024-06-25 15:32:46 +0900

    111

--- groupware/src/main/java/smile/office/groupware/admin/service/AdminService.java
+++ groupware/src/main/java/smile/office/groupware/admin/service/AdminService.java
@@ -1,4 +1,17 @@
 package smile.office.groupware.admin.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import smile.office.groupware.admin.dao.AdminDao;
+import smile.office.groupware.admin.vo.AdminVo;
+
+@Service
+@RequiredArgsConstructor
 public class AdminService {
+
+    private final AdminDao dao;
+
+    public AdminVo login(AdminVo vo) {
+        return dao.login(vo);
+    }
 }
